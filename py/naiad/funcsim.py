4b19bc9ffbc93b7e007b7a94e02ab943a9580d2b
Will Turner <rwturner17@gmail.com>
2023-06-25 16:02:20 -0400

    improve names and docstrings

--- py/naiad/funcsim.py
+++ py/naiad/funcsim.py
@@ -1,14 +1,21 @@
 from typing import Callable, Tuple
 import numpy.typing as npt
 import numpy as np
-from scipy.interpolate import interp1d
+from scipy.interpolate import interp1d  # type: ignore
 
 EPS = 1e-3
 
-RealFn = Callable[[float], float]
+FloatFn = Callable[[float], float]
+FloatArray = npt.NDArray[np.float64]
 
 
-class RealFn2:
+class FloatFn2:
+    """
+    A function that takes a float and returns a float.
+
+    Carries metadata to help with memoization
+    """
+
     def __init__(self, fn: Callable[[float], float], functional_name: str = "UNK"):
         self.fn = fn
         self.functional_name = functional_name
@@ -20,37 +27,46 @@ class RealFn2:
         return f"{self.functional_name} ∘ {str(self.fn)}"
 
 
-def arc_length_array(
-    x_axis: npt.NDArray[np.float64], y_axis: npt.NDArray[np.float64]
-) -> npt.NDArray[np.float64]:
+def arc_length_array(xcoords: FloatArray, ycoords: FloatArray) -> FloatArray:
     """
-    Get the arc length of a curve given by x_axis and y_axis.
+    Get the arc length, as an array, of a plane curve specified at xcoords and ycoords.
     """
-    assert len(x_axis) == len(y_axis)
-    dx = np.diff(x_axis)
-    dy = np.diff(y_axis)
+    assert len(xcoords) == len(ycoords)
+    dx = np.diff(xcoords)
+    dy = np.diff(ycoords)
     ss = np.cumsum(np.sqrt(dx**2 + dy**2))
-    result = np.hstack([[0], ss])
+    result = np.hstack([[0], ss])  # add zero to make the array the same length
     return result
 
 
-def array_to_function(
-    x_axis: npt.NDArray[np.float64], y_axis: npt.NDArray[np.float64]
-) -> RealFn:
-    interpolator: RealFn = interp1d(
-        x_axis, y_axis, kind="cubic", fill_value="extrapolate"
+def array_to_function(x_array: FloatArray, y_array: FloatArray) -> FloatFn:
+    """
+    Given two arrays of floats, return a function that interpolates between them.
+
+    The function computes y as a function of x.
+    """
+    interpolator: FloatFn = interp1d(
+        x_array, y_array, kind="cubic", fill_value="extrapolate"
     )
     return interpolator
 
 
-def derivative(fn: RealFn, eps: float = EPS) -> RealFn:
+def derivative(fn: FloatFn, eps: float = EPS) -> FloatFn:
+    """
+    Given a function, return a function that computes the derivative of the function.
+    """
+
     def result(x: float, eps: float = eps) -> float:
         return (fn(x + eps) - fn(x - eps)) / (2 * eps)
 
     return result
 
 
-def xy_to_curvature(x_fn: RealFn, y_fn: RealFn) -> RealFn:
+def xy_to_curvature(x_fn: FloatFn, y_fn: FloatFn) -> FloatFn:
+    """
+    Given two functions that compute x and y as a function of s,
+    return a function that computes the curvature as a function of s.
+    """
     dx_fn = derivative(x_fn)
     ddx_fn = derivative(dx_fn)
     dy_fn = derivative(y_fn)
@@ -74,8 +90,15 @@ def xy_to_curvature(x_fn: RealFn, y_fn: RealFn) -> RealFn:
 
 
 def curvature_evolve(
-    curvature_fn: RealFn, delta_t: float, alpha: float, vv: float
-) -> RealFn:
+    curvature_fn: FloatFn, delta_t: float, alpha: float, vv: float
+) -> FloatFn:
+    """
+    Given a function that computes curvature as a function of s at some initial time,
+        and a timestamp delta_t,
+    return a function that computes curvature as a function of s
+        after delta_t has elapsed.
+    """
+
     def result(ss: float) -> float:
         """
         Evolve curvature forward in time.
@@ -86,23 +109,33 @@ def curvature_evolve(
     return result
 
 
-def curvature_to_xy(curvature_fn: RealFn, eps: float = 1e-2) -> Tuple[RealFn, RealFn]:
+def curvature_to_xy(
+    curvature_fn: FloatFn, eps: float = 1e-2
+) -> Tuple[FloatFn, FloatFn]:
+    """
+    Given a function that computes curvature as a function of s,
+    return two functions that compute x and y as a function of s.
+    """
     theta_fn = integrate(curvature_fn, 0, eps)
 
-    costheta_fn: RealFn = lambda ss: np.cos(theta_fn(ss))  # noqa: E731
+    costheta_fn: FloatFn = lambda ss: np.cos(theta_fn(ss))  # noqa: E731
     x_fn = integrate(costheta_fn, 0, eps)
 
-    sintheta_fn: RealFn = lambda ss: np.sin(theta_fn(ss))  # noqa: E731
+    sintheta_fn: FloatFn = lambda ss: np.sin(theta_fn(ss))  # noqa: E731
     y_fn = integrate(sintheta_fn, 0, eps)
 
     return x_fn, y_fn
 
 
-def sample(fn: RealFn, values: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+def sample(fn: FloatFn, values: FloatArray) -> FloatArray:
+    """
+    Given a function and an array of values,
+    return an array of the function evaluated at those values.
+    """
     return np.array([fn(v) for v in values])
 
 
-def integrate_strip(fn: RealFn, x0: float, x1: float) -> float:
+def integrate_strip(fn: FloatFn, x0: float, x1: float) -> float:
     """
     Integrate a single strip using Simpson's rule
     """
@@ -110,14 +143,14 @@ def integrate_strip(fn: RealFn, x0: float, x1: float) -> float:
     return (x1 - x0) / 6 * (fn(x0) + 4 * fn(mid) + fn(x1))
 
 
-def integrate(fn: RealFn, x0: float = 0, eps: float = EPS) -> RealFn:
+def integrate(fn: FloatFn, x0: float = 0, eps: float = EPS) -> FloatFn:
     """
     Given a real function and an initial value, return a function that
     takes a float x1 and computes the integral of the function from x0 to x1.
     """
 
     def integral(x1: float) -> float:
-        strips: npt.NDArray[np.float64] = np.arange(x0, x1, eps)
+        strips: FloatArray = np.arange(x0, x1, eps)
         return np.sum(
             [
                 integrate_strip(fn, strips[i], strips[i + 1])
